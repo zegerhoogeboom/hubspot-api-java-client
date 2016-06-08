bfdff3f1d68c0788d25900d1ee2dc718f8da8927
Zeger Hoogeboom <zeger.hoogeboom@gmail.com>
2016-06-08 12:27:58 +0200

    Refactoring Hubspot

--- hubspot-api-java-client/src/main/java/farm/shangrila/hubspt/api/HBSPTUnsuccessfulResponseHandler.java
+++ hubspot-api-java-client/src/main/java/farm/shangrila/hubspt/api/HBSPTUnsuccessfulResponseHandler.java
@@ -1,44 +1,44 @@
 package farm.shangrila.hubspt.api;
 
-import java.io.IOException;
-import java.util.logging.Logger;
-
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.HttpUnsuccessfulResponseHandler;
 import com.google.api.client.json.JsonFactory;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+
 public class HBSPTUnsuccessfulResponseHandler implements HttpUnsuccessfulResponseHandler {
 
-	private static final Logger log = Logger.getLogger(HBSPTUnsuccessfulResponseHandler.class.getName());
+    private static final Logger log = Logger.getLogger(HBSPTUnsuccessfulResponseHandler.class.getName());
 
-	private JsonFactory jsonFactory;
+    private JsonFactory jsonFactory;
 
-	public HBSPTUnsuccessfulResponseHandler(JsonFactory jsonFactory) {
-		// TODO Auto-generated constructor stub
-		this.jsonFactory = jsonFactory;
-	}
+    public HBSPTUnsuccessfulResponseHandler(JsonFactory jsonFactory) {
+        // TODO Auto-generated constructor stub
+        this.jsonFactory = jsonFactory;
+    }
 
-	public boolean handleResponse(HttpRequest request, HttpResponse response, boolean supportsRetry)
-			throws IOException {
+    public boolean handleResponse(HttpRequest request, HttpResponse response, boolean supportsRetry)
+            throws IOException {
 
-		String responseAsString = response.parseAsString();
-		log.warning(request.getUrl() + " generate HBSPTResponseException: " + responseAsString);
+        String responseAsString = response.parseAsString();
+        log.warning(request.getRequestMethod() + ": " + request.getUrl() + " generate HBSPTResponseException: " + responseAsString);
 
-		HBSPTJsonErrorResponse hbsptJsonErrorReponse = null;
+        HBSPTJsonErrorResponse hbsptJsonErrorReponse = null;
 
-		try {
-			hbsptJsonErrorReponse = jsonFactory.fromString(responseAsString, HBSPTJsonErrorResponse.class);
-		} catch (IOException e) {
-			String errorMessage = "IOException during parse of HBSPTJsonErrorResponse: " + e.getMessage();
-			log.severe(errorMessage);
+        try {
+            hbsptJsonErrorReponse = jsonFactory.fromString(responseAsString, HBSPTJsonErrorResponse.class);
+        } catch (IOException e) {
+            String errorMessage = "IOException during parse of HBSPTJsonErrorResponse: " + e.getMessage();
+            log.severe(errorMessage);
 
-			// fill with base infos
-			hbsptJsonErrorReponse = new HBSPTJsonErrorResponse();
-			hbsptJsonErrorReponse.setMessage(errorMessage);
-			hbsptJsonErrorReponse.setStatus(String.valueOf(response.getStatusCode()));
-		}
+            // fill with base infos
+            hbsptJsonErrorReponse = new HBSPTJsonErrorResponse();
+            hbsptJsonErrorReponse.setMessage(errorMessage);
+            hbsptJsonErrorReponse.setStatus(String.valueOf(response.getStatusCode()));
+        }
 
-		throw new HBSPTResponseException(responseAsString, hbsptJsonErrorReponse);
-	}
+        throw new HBSPTResponseException(responseAsString, hbsptJsonErrorReponse);
+    }
 }
